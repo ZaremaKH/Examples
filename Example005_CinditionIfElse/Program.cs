2b91b1fa7b626d576b8cacb92ad6458fc531687b
Zarema <kzariema@mail.ru>
2023-02-09 20:00:09 +0300

    add Example 005 Condition If Else

---
+++ Example005_CinditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "зарема")
+{
+    Console.WriteLine("Ура, это же ЗАРЕМА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
